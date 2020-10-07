c6a88e666817a773ddd9619176a9ffd0bf511953
Fajar Siddiq <fajar_siddiq@hotmail.com>
2020-10-08 01:31:23 +0800

    Update sectionFour.js

--- src/components/sectionFour.js
+++ src/components/sectionFour.js
@@ -6,7 +6,7 @@ const SectionFour = () => {
     return (
         <div className="section-four">
                 <div className="heading">
-                    <h2>🚀 Launched on Product Hunt</h2>
+                    <h2>Launched on Product Hunt</h2>
                 </div>
                 <iframe style={{border: 'none'}} src="https://cards.producthunt.com/cards/posts/151790?v=1" width="450" height="350" frameborder="0" scrolling="no" allowfullscreen></iframe>
                 <br/>
